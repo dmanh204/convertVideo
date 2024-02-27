6f62703ad5ed210ca82936e795534870298ddbd4
dmanh204 <115561854+dmanh204@users.noreply.github.com>
2024-02-27 11:36:31 +0700

    convert and cipher video (Manh)

--- conv.py
+++ conv.py
@@ -1,7 +1,30 @@
 import numpy as np
-data = np.fromfile('big_buck_bunny_720p_2mb.mp4', dtype=np.dtype('B'))
-# with open('big_buck_bunny_720p_2mb.mp4', 'rb') as f:
-#     data = f.read()
-output = ''.join(format(byte, '08b') for byte in data)
-with open('bin.txt', 'w') as file:
+import sys
+import math
+sys.path.insert(0, 'F:/TaiLieuHocTap/sip/StreamCipher/lightWeightStreamCipher')
+import lightweight as lw
+
+def xor_encrypt_decrypt(bin_np, key):
+    key_np = np.array(key, dtype=np.uint8)
+    key_np = np.resize(key_np, bin_np.shape) # Resize key stream  equal to binary stream.
+
+    result = np.bitwise_xor(bin_np, key_np)
+    return result
+
+# File mp4 can ma hoa
+mp4 = 'sample.mp4'
+data = np.fromfile(mp4, dtype=np.dtype('B'))# Doc tu file binary .mp4 ra numpy array
+
+# Khoi tao ma dong
+key = 0x1234567890abcdef
+ma = lw.light(key)
+sKey = []
+for _ in range(math.ceil(data.size / 32)):
+    sKey.extend(ma.run())
+
+# Tien hanh ma hoa video bang ma dong
+ketqua = xor_encrypt_decrypt(data, sKey)
+output = ''.join(format(byte, '08b') for byte in ketqua)# chuyen cac phan tu trong numpy array tu dang binary
+# sang dang string roi ghep vao output.
+with open('Test.txt', 'w') as file: # Viet ket qua vao file .txt
     file.write(output)
--- reverse.py
+++ reverse.py
@@ -1,6 +1,33 @@
 import numpy as np
-with open('bin.txt', 'r') as file:
+import math
+import sys
+sys.path.insert(0, 'F:/TaiLieuHocTap/sip/StreamCipher/lightWeightStreamCipher')
+import lightweight as lw
+
+def xor_encrypt_decrypt(bin_np, key):
+    key_np = np.array(key, dtype=np.uint8) # convert to numpy array
+    key_np = np.resize(key_np, bin_np.shape)  # Resize key stream  equal to binary stream.
+
+    result = np.bitwise_xor(bin_np, key_np)
+    return result
+
+# Read file txt
+with open('Test.txt', 'r') as file:
     stringData = file.read()
-binary_data = bytes(int(stringData[i:i+8], 2) for i in range(0, len(stringData), 8))
-with open('reconstructVideo.mp4', 'wb') as file:
-    file.write(binary_data)
+# For each 8 characters in string, convert them into an integer number and the string is converted to a tuple
+binary_data = tuple(int(stringData[i:i+8], 2) for i in range(0, len(stringData), 8))
+# Convert tuple to numpy array for decipher
+data = np.array(binary_data, dtype=np.uint8)  # convert to numpy array
+# Khoi tao ma dong
+key = 0x1234567890abcdef
+ma = lw.light(key)
+sKey = []
+for _ in range(math.ceil(data.size / 32)):
+    sKey.extend(ma.run())
+
+# Tien hanh giai ma hoa video bang ma dong
+ketqua = xor_encrypt_decrypt(data, sKey)
+# Viet vao file binary
+with open('testVideo.mp4', 'wb') as file:
+    #file.write(binary_data)
+    ketqua.tofile(file)
