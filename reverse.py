b590dc94d784cbc219011ca5964699ef482c008d
dmanh204 <115561854+dmanh204@users.noreply.github.com>
2024-02-29 11:44:42 +0700

    update them salsa20 va sosemanuk

--- reverse.py
+++ reverse.py
@@ -3,7 +3,12 @@ import math
 import sys
 sys.path.insert(0, 'F:/TaiLieuHocTap/sip/StreamCipher/lightWeightStreamCipher')
 import lightweight as lw
-
+sys.path.insert(0, 'F:/TaiLieuHocTap/sip/StreamCipher/RC4')
+import Rc4 as rc4
+sys.path.insert(0, 'F:/TaiLieuHocTap/sip/StreamCipher/Salsa20')
+import salsa20 as s20
+sys.path.insert(0, 'F:/TaiLieuHocTap/sip/StreamCipher/Sosemanuk')
+import SOSEMANUK as ssmn
 def xor_encrypt_decrypt(bin_np, key):
     key_np = np.array(key, dtype=np.uint8) # convert to numpy array
     key_np = np.resize(key_np, bin_np.shape)  # Resize key stream  equal to binary stream.
@@ -12,22 +17,42 @@ def xor_encrypt_decrypt(bin_np, key):
     return result
 
 # Read file txt
-with open('Test.txt', 'r') as file:
+with open('TestSosemanuk.txt', 'r') as file:
     stringData = file.read()
 # For each 8 characters in string, convert them into an integer number and the string is converted to a tuple
 binary_data = tuple(int(stringData[i:i+8], 2) for i in range(0, len(stringData), 8))
 # Convert tuple to numpy array for decipher
 data = np.array(binary_data, dtype=np.uint8)  # convert to numpy array
 # Khoi tao ma dong
-key = 0x1234567890abcdef
-ma = lw.light(key)
 sKey = []
-for _ in range(math.ceil(data.size / 32)):
-    sKey.extend(ma.run())
+print("1. RC4 \n2. Salsa20 \n3. Sosemanuk \n4. Taha \n5. China \n6. Proposed")
+choice = int(input())
+if choice == 1:
+    key = [100, 90, 176, 211, 45, 73, 192, 153, 235, 111, 74, 32]
+    k = rc4.RC4(key)
+    for i in range(data.size):
+        sKey.append(k.Generate())
+if choice == 2:
+    key = "9e33bd7c6068e87fbe8aa2e7c5628f0d3699a5c5f108fdc1e995982028cbfb7c"
+    nonce = "947cb4326dba6e79"
+    ma = s20.Salsa20(key, nonce)
+    for _ in range(math.ceil(data.size / 64)): # Vi mot lan chay salsa20 sinh ra 16 gia tri 4-byte = 64 byte
+        sKey.extend(ma.run())
+if choice == 3:
+    key = 0x2B32CA3BD845A26FF5005EDCB56D94BAF6CDE6936AC4CFB91A421DDC93DD6EB7
+    initialVector = 0x72D242D18F56E1448BA1996C8344D669
+    ma = ssmn.SOSEMANUK(key, initialVector)
+    for _ in range(math.ceil(data.size / 16)): # Vi mot lan chay SOSEMANUK sinh ra 4 gia tri 4-byte
+        sKey.extend(ma.run())
+if choice == 6:
+    key = 0x1234567890abcdef
+    ma = lw.light(key)
+    for _ in range(math.ceil(data.size / 32)):
+        sKey.extend(ma.run())
 
 # Tien hanh giai ma hoa video bang ma dong
 ketqua = xor_encrypt_decrypt(data, sKey)
 # Viet vao file binary
-with open('testVideo.mp4', 'wb') as file:
+with open('testVideoSosemanuk.mp4', 'wb') as file:
     #file.write(binary_data)
     ketqua.tofile(file)
